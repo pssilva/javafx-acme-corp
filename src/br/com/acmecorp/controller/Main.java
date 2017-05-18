10059d9e233b8a2fdafb6bf40442afd90372b9d2
Paulo Sérgio da Silva <pss1suporte@gmail.com>
2017-05-18 19:25:51 -0300

    Estrutura da Aplicação: Resource

--- src/br/com/acmecorp/controller/Main.java
+++ src/br/com/acmecorp/controller/Main.java
@@ -1,25 +1,58 @@
 package br.com.acmecorp.controller;
 	
+import java.util.List;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../view/main.fxml"));
-			Scene scene = new Scene(root,500,300);
-			scene.getStylesheets().add(getClass().getResource("../assets/css/application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			final StageStyle stageStyle = configStageStyle();
+			
+			System.out.println(getClass().getResource("").getPath());
+			
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/layouts/main.fxml"));
+			BorderPane rootGroup = fxmlLoader.load();
+	        final SampleController controller = fxmlLoader.getController();
+	        controller.setStage(primaryStage);
+	        controller.setupBinding(stageStyle);
+	        
+	        Scene scene = new Scene(rootGroup, 500, 300);
+	        scene.setFill(Color.TRANSPARENT);
+	        scene.getStylesheets().add(getClass().getResource("/assets/css/application.css").toExternalForm());
+	        
+	        primaryStage.setScene(scene);
+	        primaryStage.setOnCloseRequest(we -> System.out.println("Stage is closing"));
+	        primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private StageStyle configStageStyle() {
+        StageStyle stageStyle = StageStyle.DECORATED;
+        List<String> unnamedParams = getParameters().getUnnamed();
+        if (unnamedParams.size() > 0) {
+            String stageStyleParam = unnamedParams.get(0);
+            if (stageStyleParam.equalsIgnoreCase("transparent")) {
+                stageStyle = StageStyle.TRANSPARENT;
+            } else if (stageStyleParam.equalsIgnoreCase("undecorated")) {
+                stageStyle = StageStyle.UNDECORATED;
+            } else if (stageStyleParam.equalsIgnoreCase("utility")) {
+                stageStyle = StageStyle.UTILITY;
+            }
+        }
+        return stageStyle;
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
--- src/br/com/acmecorp/controller/SampleController.java
+++ src/br/com/acmecorp/controller/SampleController.java
@@ -1,5 +1,18 @@
 package br.com.acmecorp.controller;
 
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 public class SampleController {
+
+	public void setStage(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setupBinding(StageStyle stageStyle) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
