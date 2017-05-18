572da2442d8161473dec22b9757e4fcb086d7ae9
Paulo Sérgio da Silva <pss1suporte@gmail.com>
2017-05-18 16:31:48 -0300

    Estrutura da Aplicação: MVC

---
+++ src/br/com/acmecorp/controller/Main.java
@@ -0,0 +1,27 @@
+package br.com.acmecorp.controller;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../view/main.fxml"));
+			Scene scene = new Scene(root,500,300);
+			scene.getStylesheets().add(getClass().getResource("../assets/css/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/br/com/acmecorp/controller/SampleController.java
@@ -0,0 +1,5 @@
+package br.com.acmecorp.controller;
+
+public class SampleController {
+	
+}
